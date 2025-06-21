18b5772606608dd2d2711be590026952640e3807
Mayank Balmoori <balmoorimayank@gmail.com>
2025-06-21 21:13:37 +0530

    changed the port of ws

--- week25/apps/ws/src/server.ts
+++ week25/apps/ws/src/server.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from "ws";
 import { client } from '@repo/db/prismaClient'
 
-const wss = new WebSocketServer({port: 8080})
+const wss = new WebSocketServer({port: 3001})
 
 wss.on("connection",async (ws)=>{
     await client.user.create({
